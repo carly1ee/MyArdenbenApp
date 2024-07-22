a57f27f03176788e6b76aaf3322a828016dfe935
ardenben <ardenben@student.21-school.ru>
2024-07-22 12:53:43 +0300

    second

--- MyArdenbenApp/ViewController.swift
+++ MyArdenbenApp/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var switcher: UISwitch!
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var subView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    
+    @IBAction func switcher(_ sender: UISwitch) {
+        if sender.isOn { subView.backgroundColor = UIColor.blue
+        }
+        else {
+            subView.backgroundColor = UIColor.systemGreen
+        }
+    }
 }
 
